e0025b036e71a69ebc80a976e5c350f4c64a22e0
Jaqueline <59448111+Jaque-81@users.noreply.github.com>
2021-11-22 11:41:51 +0000

    Melhorando a legibilidade do código.

--- main.py
+++ main.py
@@ -22,15 +22,18 @@ print(50 * "*")
 numero_secreto = 42
 chute_str = input("Digite seu número: ")
 chute = int(chute_str)
+acertou = numero_secreto == chute
+chute_maior = numero_secreto > chute
+chute_menor = numero_secreto < chute
 
 print("Você digitou ", chute)
 
-if (numero_secreto == chute):
+if (acertou):
     print("Você acertou!")
 else:
-  if(chute > numero_secreto):
+  if(chute_maior):
     print("Você errou! O chute foi maior que o número secreto.")
-  elif(chute < numero_secreto):
+  elif(chute_menor):
     print("Você errou! O chute foi menor que o número secreto.")
 
 print("Você errou")
