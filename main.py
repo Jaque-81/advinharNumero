38634dfeb729d97602d033e3f818df47b1382773
Jaqueline <59448111+Jaque-81@users.noreply.github.com>
2021-11-26 11:53:38 +0000

    For tentativas + 1.

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ rodada = 1
 numero_secreto = 42
 total_de_tentativas = 3
 
-for rodada in range (1, total_de_tentativas) :
+for rodada in range (1, total_de_tentativas + 1) :
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
 
     chute_str = input("Digite o seu número: ")
