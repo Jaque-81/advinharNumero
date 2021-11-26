04add64b4c90d05c9980a3d4c1d3d356eb888efc
Jaqueline <59448111+Jaque-81@users.noreply.github.com>
2021-11-26 11:52:21 +0000

    Usando o for.

--- main.py
+++ main.py
@@ -8,15 +8,13 @@ rodada = 1
 
 numero_secreto = 42
 total_de_tentativas = 3
-rodada = 1
 
-while (rodada <= total_de_tentativas):
+for rodada in range (1, total_de_tentativas) :
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
 
     chute_str = input("Digite o seu número: ")
     print("Você digitou " , chute_str)
     chute = int(chute_str)
-rodada = rodada + 1
 
 acertou = numero_secreto == chute
 chute_maior = numero_secreto > chute
