d5d4417c3daf31b3217cbf0d1e49fb07be0982fc
Jaqueline <59448111+Jaque-81@users.noreply.github.com>
2021-12-10 10:52:53 +0000

    Apresentando os pontos quando o jogador acerta.

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ print(50 * "*")
 
 numero_secreto = random.randrange(1, 21)
 total_tentativas = 0
+pontos = 1000
+
 
 print("Qual o nível de dificuldade?\n(1)Fácil (2)Médio (3)Difícil")
 
@@ -33,12 +35,14 @@ for rodada in range(1, total_tentativas + 1):
     menor = chute < numero_secreto
 
     if (acertou):
-        print("Você acertou!")
+        print("Você acertou e fez {} pontos ".format(pontos))
         break
     else:
         if (maior):
             print("Você errou! O seu chute foi maior do que o número secreto.")
         elif (menor):
             print("Você errou! O seu chute foi menor do que o número secreto.")
+    pontos_perdidos = abs(numero_secreto - chute)
+    pontos = pontos - pontos_perdidos
 
 print("Fim do jogo")
